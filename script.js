94ee74800184cfb651485020c4ccfc261a0490ff
Lorenzo Brizolla <lorenzobrizolla789@gmail.com>
2025-08-28 16:54:06 -0300

    Resposta do exercício

---
+++ script.js
@@ -0,0 +1,6 @@
+const box = document.querySelector(".box");
+const botao = document.querySelector(".btn");
+
+botao.addEventListener("click", () => {
+    box.style.backgroundColor = "#ffd43d";
+});
